c7112364efd3ccd052a9f885bcd737550a6ad1d1
drimailorr <drimailorr@mail.ru>
2016-02-18 19:10:44 +0200

    17

--- vagrant/ansibleprovisioning.sh
+++ vagrant/ansibleprovisioning.sh
@@ -4,4 +4,5 @@ yum install epel-release -y
 yum install ansible -y
 yum install git -y
 git clone https://github.com/drimailorr/datarobot.git ~/git_repo
+cd ~/git_repo/ansible_repo/
 ansible-playbook ~/git_repo/ansible_repo/site.yml
--- vagrant/appprovisioning.sh
+++ vagrant/appprovisioning.sh
@@ -3,3 +3,5 @@ yum install -y epel-release
 yum install -y python-pip
 pip install --upgrade pip
 pip install flask pymongo
+service iptables stop
+chkconfig iptables off
