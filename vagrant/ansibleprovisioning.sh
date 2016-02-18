71c1d8796301ca75a4eebb216a51e88a95d181e1
drimailorr@mail.ru <root@ansibleserver.roma.local>
2016-02-18 16:00:05 +0000

    webserver completed

--- vagrant/ansibleprovisioning.sh
+++ vagrant/ansibleprovisioning.sh
@@ -3,5 +3,5 @@
 yum install epel-release -y
 yum install ansible -y
 yum install git -y
-git clone https://github.com/drimailorr/datarobot.git ~/ansible_repo
-#ansible-playbook ~/ansible_repo/site.yml
+git clone https://github.com/drimailorr/datarobot.git ~/git_repo
+ansible-playbook ~/git_repo/ansible_repo/site.yml
